2fd07fa5575e3339e3392b03661d81d9d84d4331
César Tzoc <csartalva01@gmail.com>
2026-02-19 20:44:37 -0600

    MRL: add exponential and potential models for class 19022026

---
+++ mrl/class19022026/class19022026-exponencial-model.R
@@ -0,0 +1,59 @@
+# Exponencial model
+
+vehicles <- read.xlsx("mrl/class19022026/distancia_frenado.xlsx", sheet = 1)
+
+# plot distance vs speed
+plot(
+  vehicles$velocidad,
+  vehicles$distancia,
+  main = "Distancia de Frenado vs Velocidad",
+  xlab = "Velocidad (km/h)",
+  ylab = "Distancia de Frenado (m)",
+  pch = 19,
+  col = "blue"
+)
+
+vehicles$log_distance <- log(vehicles$distancia)
+
+model <-lm(log_distance ~ velocidad, data = vehicles)
+
+# model$coefficients[[1]] # Intercept
+# model$coefficients[[2]] # Slope
+# summary(model)
+
+vehicles$predicted_log_distance <- predict(model)
+plot(
+  vehicles$velocidad,
+  vehicles$predicted_log_distance,
+  main = "Predicted Log Distance vs Velocidad",
+  xlab = "Velocidad (km/h)",
+  ylab = "Predicted Log Distance",
+  pch = 19,
+  col = "red"
+)
+
+formula <- function(x) {
+  exp(model$coefficients[[1]] + model$coefficients[[2]] * x)
+  # exp(model$coefficients[[1]]) * exp(model$coefficients[[2]] * x)
+}
+
+# plot real data and model
+plot(
+  vehicles$velocidad,
+  vehicles$distancia,
+  xlim = c(4, 25),
+  ylim = c(0, 120),
+  main = "Distancia de Frenado vs Velocidad",
+  xlab = "Velocidad (km/h)",
+  ylab = "Distancia de Frenado (m)",
+  pch = 19,
+  col = "red",
+)
+par(new = T)
+plot(
+  vehicles$velocidad,
+  formula(vehicles$velocidad),
+  col = "blue",
+  type = "l",
+  lwd = 2
+)
---
+++ mrl/class19022026/class19022026-potencial-model.R
@@ -0,0 +1,35 @@
+# multiplicative potential model
+vehicles <- read.xlsx("mrl/class19022026/distancia_frenado.xlsx", sheet = 1)
+
+vehicles$log_distance <- log(vehicles$distancia)
+vehicles$log_velocity <- log(vehicles$velocidad)
+
+model <- lm(log_distance ~ log_velocity, data = vehicles)
+model
+summary(model)
+
+vehicles$predicted_log_distance <- predict(model)
+
+formula <- function(x) {
+    exp(model$coefficients[[1]] + model$coefficients[[2]] * log(x))
+}
+
+plot(
+    vehicles$velocidad,
+    vehicles$distancia,
+    xlim = c(4, 25),
+    ylim = c(0, 120),
+    main = "Distancia de Frenado vs Velocidad",
+    xlab = "Velocidad (km/h)",
+    ylab = "Distancia de Frenado (m)",
+    pch = 19,
+    col = "red"
+)
+par(new = T)
+plot(
+    vehicles$log_velocity,
+    formula(vehicles$velocidad),
+    col = "green",
+    type = "l",
+    lwd = 2
+)
